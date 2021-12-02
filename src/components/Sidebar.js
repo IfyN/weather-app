67c8f430bc797c1125ea2402e88f0baa37d39d06
IfyN <ifynwosu0@gmail.com>
2021-12-02 17:54:56 +0100

    add sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledSidebar } from "./Sidebar.styles";
+
+function Sidebar() {
+  return (
+    <StyledSidebar>
+      <div className="body">ygygyf</div>
+    </StyledSidebar>
+  );
+}
+
+export default Sidebar;
