cf2c45ece4d611f54435dba0b5ce1ee284b7d11c
IfyN <ifynwosu0@gmail.com>
2022-02-16 00:37:22 +0100

    add component for daily warther updates

---
+++ src/components/DailyWeather.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledDailyWeather } from "./DailyWeather.styles";
+
+function DailyWeather() {
+  return <StyledDailyWeather></StyledDailyWeather>;
+}
+
+export default DailyWeather;
---
+++ src/components/DailyWeather.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledDailyWeather = styled.div`
+  background-color: #100e1d;
+  min-height: 100vh;
+`;
