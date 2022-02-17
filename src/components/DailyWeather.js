9a44a4742577ada530038fb254d6f7cdd1233ab5
IfyN <ifynwosu0@gmail.com>
2022-02-17 10:31:28 +0100

    style diaily weather component

--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -2,7 +2,55 @@ import React from "react";
 import { StyledDailyWeather } from "./DailyWeather.styles";
 
 function DailyWeather() {
-  return <StyledDailyWeather></StyledDailyWeather>;
+  return (
+    <StyledDailyWeather>
+      <div className="container">
+        <div className="date-one">
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+        </div>
+
+        <div className="date-one">
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+        </div>
+
+        <div className="date-one">
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+        </div>
+
+        <div className="date-one">
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+        </div>
+
+        <div className="date-one">
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+          <p> Tomorrow </p>
+        </div>
+      </div>
+    </StyledDailyWeather>
+  );
 }
 
 export default DailyWeather;
--- src/components/DailyWeather.styles.js
+++ src/components/DailyWeather.styles.js
@@ -1,6 +1,25 @@
 import styled from "styled-components";
 
 export const StyledDailyWeather = styled.div`
-  background-color: #100e1d;
-  min-height: 100vh;
+  width: 90%;
+  margin: 0 auto;
+  height: 300px;
+
+  .container {
+    display: grid;
+    justify-content: center;
+    grid-template-columns: repeat(5, 200px);
+    gap: 30px;
+    padding: 10px;
+    margin-top: 12%;
+
+    @media screen and (max-width: 768px) {
+      grid-template-columns: repeat(2, 1fr);
+      gap: 20px;
+    }
+  }
+
+  .date-one {
+    background-color: #1e213a;
+  }
 `;
