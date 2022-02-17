bc008e9bfcdd9cf730bf37afdf0b1d3464de1ff1
IfyN <ifynwosu0@gmail.com>
2022-02-17 10:31:49 +0100

    add dashboard comp, dashboard styles

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyledDashboard } from "./Dashboard.styles";
+import DailyWeather from "./DailyWeather";
+
+function Dashboard() {
+  return (
+    <StyledDashboard>
+      <p>ggggggedgdgsgeyfgysgy</p>
+      <DailyWeather />
+    </StyledDashboard>
+  );
+}
+
+export default Dashboard;
---
+++ src/components/Dashboard.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledDashboard = styled.div`
+  background-color: #100e1d;
+  min-height: 100vh;
+
+  p {
+    color: white;
+  }
+`;
