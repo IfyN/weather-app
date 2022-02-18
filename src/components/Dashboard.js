8f572cfceaac72e529263c06addc490b547aa55b
IfyN <ifynwosu0@gmail.com>
2022-02-18 18:12:08 +0100

    clean up

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,6 @@ import DailyWeather from "./DailyWeather";
 function Dashboard() {
   return (
     <StyledDashboard>
-      <p>ggggggedgdgsgeyfgysgy</p>
       <DailyWeather />
     </StyledDashboard>
   );
