67782e3c4717c74c1d23b1d8dbd80e1d85b951ed
IfyN <ifynwosu0@gmail.com>
2022-02-18 18:11:59 +0100

    map data array

--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -1,53 +1,20 @@
 import React from "react";
 import { StyledDailyWeather } from "./DailyWeather.styles";
-
+import { Infobox } from "./Infobox";
 function DailyWeather() {
   return (
     <StyledDailyWeather>
       <div className="container">
-        <div className="date-one">
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-        </div>
-
-        <div className="date-one">
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-        </div>
-
-        <div className="date-one">
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-        </div>
-
-        <div className="date-one">
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-        </div>
-
-        <div className="date-one">
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-          <p> Tomorrow </p>
-        </div>
+        {Infobox.map((item, idx) => {
+          return (
+            <div className="date-one" key={idx}>
+              <p> {item.date} </p>
+              <p> {item.cent} </p>
+              <img src={item.img} />
+              <p> {item.fan} </p>
+            </div>
+          );
+        })}
       </div>
     </StyledDailyWeather>
   );
