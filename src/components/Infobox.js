5e671f406c69d1be03bef7eb2b04eadcd095a263
IfyN <ifynwosu0@gmail.com>
2022-02-18 14:09:42 +0100

    add weather details data

---
+++ src/components/Infobox.js
@@ -0,0 +1,32 @@
+export const Infobox = [
+  {
+    date: "Tomorrow",
+    cent: "16°c",
+    fan: "11°c",
+    img: "https://res.cloudinary.com/ifeoma/image/upload/v1639659828/Sleet_p7qwpq.png",
+  },
+  {
+    date: "Tomorrow",
+    cent: "16°c",
+    fan: "11°c",
+    img: "https://res.cloudinary.com/ifeoma/image/upload/v1639659827/LightCloud_kve0ke.png",
+  },
+  {
+    date: "Tomorrow",
+    cent: "16°c",
+    fan: "11°c",
+    img: "https://res.cloudinary.com/ifeoma/image/upload/v1639659828/Shower_nsrvy9.png",
+  },
+  {
+    date: "Tomorrow",
+    cent: "16°c",
+    fan: "11°c",
+    img: "https://res.cloudinary.com/ifeoma/image/upload/v1639659828/Thunderstorm_poixpz.png",
+  },
+  {
+    date: "Tomorrow",
+    cent: "16°c",
+    fan: "11°c",
+    img: "https://res.cloudinary.com/ifeoma/image/upload/v1639659827/HeavyRain_bjzznv.png",
+  },
+];
