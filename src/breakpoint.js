e821ff2ff25ded875141a235048a14a344412aa1
IfyN <ifynwosu0@gmail.com>
2022-03-05 21:08:34 +0100

    add breakpoint for mobile responsiveness

---
+++ src/breakpoint.js
@@ -0,0 +1,24 @@
+import react from "react";
+import PropTypes from "prop-types";
+
+export const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+export const device = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+  desktop: `(max-width: ${size.desktop})`,
+};
+
+export default { size, device };
