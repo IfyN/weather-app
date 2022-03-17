135843c4292c0ecdf534d8edaca4e623aae39d3f
IfyN <ifynwosu0@gmail.com>
2022-03-17 18:36:49 +0100

    add state for input section, add toggle to button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,49 @@
-import React from "react";
+import { React, useState } from "react";
 import { StyledSidebar } from "./Sidebar.styles";
 
 function Sidebar() {
+  const [textarea, setTextarea] = useState();
+  const [search, setSearch] = useState(false);
+
+  const handleChange = (event) => {
+    setTextarea(event.target.value);
+  };
+
+  const handleSearch = () => {
+    setSearch(true);
+  };
   return (
     <StyledSidebar>
-      <div className="body">
-        <img
-          className="sidebar__image-cloud"
-          src="https://res.cloudinary.com/ifeoma/image/upload/v1639659828/Shower_nsrvy9.png"
-          alt="Shower"
-        />
+      {search ? (
+        <h1>Hello</h1>
+      ) : (
+        <div className="weather-section">
+          <div className="form">
+            <form>
+              <input
+                type="text"
+                value={textarea}
+                onChange={handleChange}
+                placeholder="Search for places"
+              />
+            </form>
 
-        <div className="weather-readings">
-          <h1>15°C</h1>
-          <h3>Shower</h3>
+            <button className="round-button" onClick={handleSearch}></button>
+          </div>
+          <div className="body">
+            <img
+              className="sidebar__image-cloud"
+              src="https://res.cloudinary.com/ifeoma/image/upload/v1639659828/Shower_nsrvy9.png"
+              alt="Shower"
+            />
+
+            <div className="weather-readings">
+              <h1>15°C</h1>
+              <h3>Shower</h3>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </StyledSidebar>
   );
 }
