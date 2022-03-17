0c8a5ee03b430a88cb8c5f107e9a64fdebaabc1c
IfyN <ifynwosu0@gmail.com>
2022-03-17 18:37:12 +0100

    ass style for input section and button toggle

--- src/components/Sidebar.styles.js
+++ src/components/Sidebar.styles.js
@@ -6,12 +6,44 @@ export const StyledSidebar = styled.div`
   display: grid;
   grid-auto-flow: column;
 
+.weather-section{
+  position: relative;
+  /* display: flex; */
+  /* margin-left: 20%; */
+}
+
+.form{
+  display: flex;
+  justify-content: space-between;
+  margin: 35px 46px 0;
+}
+
+input{
+  height: 40px;
+  width: 161px;
+  background-color: #6E707A;
+  border: none;
+}
+
+input::placeholder{
+  color: #E7E7EB;
+  text-align: center;
+  font-size: 16px;
+}
+
+.round-button{
+  height: 40px;
+  width: 40px;
+  background-color: #6E707A;
+  border-radius: 50%;
+  border:none ;
+}
 
 .body{
   display: flex;
   align-items: center;
   flex-direction: column;
-  margin-top: 40%;
+  margin-top: 25%;
 }
 
   .weather-readings{
