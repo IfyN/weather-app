d4e57aabf5a9540ded6936648bb955850220f848
IfyN <ifynwosu0@gmail.com>
2022-04-02 11:48:54 +0100

    add data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyledDashboard } from "./Dashboard.styles";
 import DailyWeather from "./DailyWeather";
+import context from "../themeContext";
 
 function Dashboard() {
+  const dataContext = useContext(context);
+  console.log(dataContext);
+  const { data, setData } = dataContext;
   return (
     <StyledDashboard>
       <DailyWeather />
+      <h1
+        style={{
+          color: "white",
+        }}>
+        {data.smaple}
+        {data.user}
+      </h1>
+      <button
+        style={{
+          backgroundColor: "red",
+        }}
+        onClick={() => {
+          setData({ ...data, user: data.user + 1 });
+        }}></button>
     </StyledDashboard>
   );
 }
