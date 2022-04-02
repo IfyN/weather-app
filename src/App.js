106670a5a16fcac97480407890f51bbfa7521f2d
IfyN <ifynwosu0@gmail.com>
2022-04-02 11:48:45 +0100

    import useState and useContext hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
+import { useContext, useState } from "react";
+import context from "./themeContext";
 
 function App() {
+  const [data, setData] = useState({ smaple: "red", user: 0 });
   return (
-    <div className="entry">
-      <Sidebar />
-      <Dashboard />
-    </div>
+    <context.Provider value={{ data, setData }}>
+      <div className="entry">
+        <Sidebar />
+        <Dashboard />
+      </div>
+    </context.Provider>
   );
 }
 
