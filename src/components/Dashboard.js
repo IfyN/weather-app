2d47c5fa6dd31804f0bc62cdfa7750e1987ee85d
IfyN <ifynwosu0@gmail.com>
2022-05-22 17:25:42 +0100

    fetch api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { StyledDashboard } from "./Dashboard.styles";
 import DailyWeather from "./DailyWeather";
 import context from "../themeContext";
+import axios from "axios";
 
 function Dashboard() {
-  const dataContext = useContext(context);
-  console.log(dataContext);
+  const [data, setData] = useState();
+  const getLocation = () => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?lat=6.5244&lon=3.3792&appid=bedafea85a3c0242ccc7b8679fbf379c"
+      )
+      .then((response) => setData(response.data));
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  // const dataContext = useContext(context);
+  // console.log(dataContext);
   // const { data, setData } = dataContext;
   return (
     <StyledDashboard>
-      <DailyWeather />
+      <DailyWeather weather={data} />
       {/* <h1
         style={{
           color: "white",
