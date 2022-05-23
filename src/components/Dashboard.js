3067da9e0a07980853cddbefc6fe489c9689bd7c
IfyN <ifynwosu0@gmail.com>
2022-05-23 23:17:47 +0100

    change api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,7 +9,7 @@ function Dashboard() {
   const getLocation = () => {
     axios
       .get(
-        "https://api.openweathermap.org/data/2.5/weather?lat=6.5244&lon=3.3792&appid=bedafea85a3c0242ccc7b8679fbf379c"
+        "https://api.openweathermap.org/data/2.5/onecall?lat=6.5244&lon=3.3792&exclude=current,minutely,hourly,alerts&appid=f8db3768585aa064c24cdd0227ba44da"
       )
       .then((response) => setData(response.data));
   };
