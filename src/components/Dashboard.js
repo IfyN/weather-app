1f757ddee3ee43cf56d3528619bb5222508470d6
IfyN <ifynwosu0@gmail.com>
2022-05-25 16:23:18 +0100

    set state, call API

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,39 +5,24 @@ import context from "../themeContext";
 import axios from "axios";
 
 function Dashboard() {
-  const [data, setData] = useState();
+  const [weatherData, setWeatherData] = useState({});
   const getLocation = () => {
     axios
       .get(
         "https://api.openweathermap.org/data/2.5/onecall?lat=6.5244&lon=3.3792&exclude=current,minutely,hourly,alerts&appid=f8db3768585aa064c24cdd0227ba44da"
       )
-      .then((response) => setData(response.data));
+      .then((response) => {
+        setWeatherData(response.data);
+      });
   };
 
   useEffect(() => {
     getLocation();
   }, []);
 
-  // const dataContext = useContext(context);
-  // console.log(dataContext);
-  // const { data, setData } = dataContext;
   return (
     <StyledDashboard>
-      <DailyWeather weather={data} />
-      {/* <h1
-        style={{
-          color: "white",
-        }}>
-        {data.smaple}
-        {data.user}
-      </h1> */}
-      {/* <button
-        style={{
-          backgroundColor: "red",
-        }}
-        onClick={() => {
-          setData({ ...data, user: data.user + 1 });
-        }}></button> */}
+      <DailyWeather climatic={weatherData.daily} />
     </StyledDashboard>
   );
 }
