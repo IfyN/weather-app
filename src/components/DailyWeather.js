0b36d9950c829fb71c3a7ad96fe6cde9128c2a18
IfyN <ifynwosu0@gmail.com>
2022-06-01 12:15:11 +0100

    code cleanup

--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -1,40 +1,40 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StyledDailyWeather } from "./DailyWeather.styles";
-import { Infobox } from "./Infobox";
 
 function DailyWeather({ climatic }) {
-  //  const displayWeather = data.map((result)=> result.daily.map(dail))
   return (
     <StyledDailyWeather>
-      <div className="container">
-        <div className="infobox-container">
-          {climatic?.map((item, idx) => {
-            return (
-              <div className="date-one" key={idx}>
-                <p className="c-dashboard_infobox-date"> {item.date} </p>
-                <img
-                  src={`http://openweathermap.org/img/wn/${item.weather[0]?.icon}@2x.png`}
-                />
-                <div className="c-dashboard_infobox-text">
-                  <p> {item.temp.max} </p>
-                  <p> {item.temp.min} </p>
-                </div>
+      <div className="infobox-container">
+        {climatic?.map((item, idx) => {
+          return (
+            <div className="date-one" key={idx}>
+              <p className="c-dashboard_infobox-date"> {item.date} </p>
+              <img
+                src={`http://openweathermap.org/img/wn/${item.weather[0]?.icon}@2x.png`}
+              />
+              <div className="c-dashboard_infobox-text">
+                <p> {item.temp.max} </p>
+                <p> {item.temp.min} </p>
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
       <div className="highlight-container">
         <h2>Today's Highlight</h2>
         <div className="c-highlight-container">
-          <div className="c-highlight-container_wind">
-            <p className="c-dashboard_infobox-date">Wind</p>
-            <div className="c-dashboard_infobox-reading">
-              <p> 7mph</p>
-            </div>
+          {climatic?.map((item, idx) => {
+            return (
+              <div className="c-highlight-container_wind" key={idx}>
+                <p className="c-dashboard_infobox-date">{item.wind_speed}</p>
+                <div className="c-dashboard_infobox-reading">
+                  <p> 7mph</p>
+                </div>
 
-            <p className="c-dashboard_infobox-unit">WSW</p>
-          </div>
+                <p className="c-dashboard_infobox-unit">WSW</p>
+              </div>
+            );
+          })}
 
           <div className="c-highlight-container_wind">
             <p className="c-dashboard_infobox-date">Humidity</p>
@@ -43,7 +43,6 @@ function DailyWeather({ climatic }) {
             </div>
           </div>
 
-          {/* <div className="c-highlight-container_humidity"></div> */}
           <div className="c-highlight-container_wind">
             <p className="c-dashboard_infobox-date">Visibilty</p>
             <div className="c-dashboard_infobox-reading">
