b61a2a46158d10885ade88779b5283e16141e032
IfyN <ifynwosu0@gmail.com>
2022-06-01 13:03:20 +0100

    add api for weather, speed, visibilty

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,9 @@ import axios from "axios";
 
 function Dashboard() {
   const [weatherData, setWeatherData] = useState({});
-  const getLocation = () => {
+  const [onedayData, setOnedayData] = useState({});
+
+  const getDaily = () => {
     axios
       .get(
         "https://api.openweathermap.org/data/2.5/onecall?lat=6.5244&lon=3.3792&exclude=current,minutely,hourly,alerts&appid=f8db3768585aa064c24cdd0227ba44da"
@@ -16,13 +18,25 @@ function Dashboard() {
       });
   };
 
+  const getOneDayWeather = () => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?lat=6.5244&lon=3.3792&appid=f8db3768585aa064c24cdd0227ba44da"
+      )
+      .then((response) => {
+        setOnedayData(response.data);
+        console.log(response);
+      });
+  };
+
   useEffect(() => {
-    getLocation();
+    getDaily();
+    getOneDayWeather();
   }, []);
 
   return (
     <StyledDashboard>
-      <DailyWeather climatic={weatherData.daily} />
+      <DailyWeather climatic={weatherData.daily} climaticDetails={onedayData} />
     </StyledDashboard>
   );
 }
