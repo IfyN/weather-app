e80496b93da0d94fa0a53e32d1eaa37ad874f803
IfyN <ifynwosu0@gmail.com>
2022-06-18 10:56:38 +0100

    move api calls up to parent component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,41 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { StyledDashboard } from "./Dashboard.styles";
 import DailyWeather from "./DailyWeather";
-import context from "../themeContext";
-import axios from "axios";
 
-function Dashboard() {
-  const [weatherData, setWeatherData] = useState({});
-  const [onedayData, setOnedayData] = useState({});
-
-  const getDaily = () => {
-    axios
-      .get(
-        "https://api.openweathermap.org/data/2.5/onecall?lat=6.5244&lon=3.3792&exclude=current,minutely,hourly,alerts&appid=f8db3768585aa064c24cdd0227ba44da"
-      )
-      .then((response) => {
-        setWeatherData(response.data);
-      });
-  };
-
-  const getOneDayWeather = () => {
-    axios
-      .get(
-        "https://api.openweathermap.org/data/2.5/weather?lat=6.5244&lon=3.3792&appid=f8db3768585aa064c24cdd0227ba44da"
-      )
-      .then((response) => {
-        setOnedayData(response.data);
-      });
-  };
-
-  useEffect(() => {
-    getDaily();
-    getOneDayWeather();
-  }, []);
-
+function Dashboard({ currentWeather, dailyWeatherData }) {
   return (
     <StyledDashboard>
-      <DailyWeather climatic={weatherData.daily} climaticDetails={onedayData} />
+      <DailyWeather
+        dailyWeatherData={dailyWeatherData}
+        currentWeather={currentWeather}
+      />
     </StyledDashboard>
   );
 }
